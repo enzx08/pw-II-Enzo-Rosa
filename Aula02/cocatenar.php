ece1737b020a744a16d1dcde5f14d1176dd4b627
Enzo Rosa <enzo.rosa4@etec.sp.gov.br>
2025-03-27 07:31:27 -0300

    AulaPW

---
+++ Aula02/cocatenar.php
@@ -0,0 +1,13 @@
+<?php
+ECHO "Hello Word!<br>";
+ECHO "Hello Word!<br>";
+ECHO "Hello Word!<br>";
+
+$nome = "Pepe";
+$sobrenome = "Carbone";
+$numero = 4;
+
+echo "${nome} <h1> ${sobrenome} </h1>";
+echo $numero . "<p> " . $nome .
+" " . $sobrenome . "</p>";
+?>
---
+++ Aula04/repeticao.php
@@ -0,0 +1,28 @@
+<?php
+
+$i =1;
+
+while ($i < 6){
+    echo $i;
+    $i++;
+    echo "<br>";
+}
+
+echo "____________ <br>";
+
+$i = 7;
+
+
+do{
+    echo $i;
+    $i++;
+    echo "<br>";
+} while ($i < 6 );
+
+echo "____________ <br>";
+
+for($x = 0; $x <= 10; $x++){
+    echo "O número é: $x <br>";
+}
+
+?>
