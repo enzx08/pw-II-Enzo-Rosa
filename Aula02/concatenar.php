3d2003409d28eee778b8ae8821e6fe66897a9661
Enzo Rosa <enzo.rosa4@etec.sp.gov.br>
2025-04-09 09:03:18 -0300

    AUlaPW

---
+++ Aula02/concatenar.php
@@ -0,0 +1,13 @@
+<?php
+  ECHO "Hello Word!<br>";
+  ECHO "Hello Word!<br>";
+  ECHO "Hello Word!<br>";
+  
+  $nome = "Enzo";
+  $sobrenome = "Rosa";
+  $numero = 4;
+  
+  echo "${nome} <h1> ${sobrenome} </h1>";
+  echo $numero . "<p> " . $nome .
+  " " . $sobrenome . "</p>";  
+?>
