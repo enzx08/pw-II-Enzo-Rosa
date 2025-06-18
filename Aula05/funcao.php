9cf40f334e36119722cd05887561fc394f70aa9b
Enzo Rosa <enzo.rosa4@etec.sp.gov.br>
2025-06-18 10:14:20 -0300

    AulaPW

---
+++ Aula05/funcao.php
@@ -0,0 +1,21 @@
+<?php
+    function myMessage() {
+        echo "Hello World! <br>";
+    }
+
+    myMessage();
+    myMessage();
+
+    function Soma($n1, $n2) {
+        echo $n1 + $n2;
+        echo "<br>";
+    }
+
+    Soma(3, 4);
+    Soma(10, 5);
+
+    function cadastrar() {
+        //vai ser uma lógica para cadastrar
+    }
+
+?>
